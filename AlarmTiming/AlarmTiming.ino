85fe4467b7c6b7823c50ddf2026cf230bcf64d5e
Saliya <dinushasaliya@gmail.com>
2021-04-05 11:15:01 +0530

    Add more features to alarm function of final

--- AlarmTiming/AlarmTiming.ino
+++ AlarmTiming/AlarmTiming.ino
@@ -3,13 +3,13 @@
 Set_LCD LCD(1, 2, 4, 5, 6, 7);
 
 
-int mode_status = 0; // variable for set the time 
+int mode_status = 0; // variable for set the time
 int alarm_min = 0;   // variables for set the alarm
 int alarm_hour = 0;
 int alarm_mode = 0;
 int alarm_time = 0;
 int alarm_stop = 0;
-
+int start_alarm = 0;
 int IsEditingMode = 0;// variable for check the mode
 
 
@@ -35,162 +35,180 @@ void loop() {
   /////////////////////////////////////////////////////////////
   //add mode button, this button chose the editing variable, minutes,ours or etc.
   ///////////////////////////////////////////////////////////////
-  if (alarm_button == 1){
+  if (alarm_button == 1) { //enter alarm set process
     alarm_mode = 1;
     alarm_stop = 1;
-    }
-  if (alarm_mode == 1){
-    
-    LCD.clear_all();
-    alarm_disp(LCD,alarm_min,alarm_hour);
+  }
+  if (alarm_mode == 1) {
 
-    if (mode==HIGH){
+    LCD.clear_all();
+    alarm_disp(LCD, alarm_min, alarm_hour);
+
+    if (mode == HIGH) {
       alarm_time = not alarm_time;
     }
 
-    if (increment == HIGH){
-      if (alarm_time == 0){
+    if (increment == HIGH) {
+      if (alarm_time == 0) {
         alarm_min += 1;
-        if (alarm_min >59){
+        if (alarm_min > 59) {
           alarm_min = 0;
         }
       }
-      if (alarm_time==1){
-        alarm_hour +=1;
-        if (alarm_hour >23){
+      if (alarm_time == 1) {
+        alarm_hour += 1;
+        if (alarm_hour > 23) {
           alarm_hour = 0;
         }
       }
     }
-    if (set_button == 1 and alarm_stop == 1){
+
+    if (decrement == HIGH) {
+      if (alarm_time == 0) {
+        alarm_min -= 1;
+        if (alarm_min < 0) {
+          alarm_min = 59;
+        }
+      }
+      if (alarm_time == 1) {
+        alarm_hour -= 1;
+        if (alarm_hour <0) {
+          alarm_hour = 23;
+        }
+      }
+    }
+
+    if (set_button == 1 and alarm_stop == 1) {
       alarm_mode = 0;
       alarm_stop = 0;
+      LCD.clear_all();
     }
     delay(600);
   }
   //////////////////////////////////////////////////////////////////////
-  else{
-  if (alarm_min == minutes and alarm_hour == hours){
-    digitalWrite(13,HIGH);delay(100);digitalWrite(13,LOW);delay(100);
-  }
-  if (alarm_button == 1){
-    digitalWrite(13,LOW);
-  }
-  if (mode == HIGH) {
-    IsEditingMode = 1;
-    if (mode_status == 5) {
-      mode_status = 0;
-    }
-    mode_status += 1;// selected minute or hours
-  }
-
-  //add increment button
-  if (increment == HIGH) {
-    if (mode_status == 1) {
-
-      minutes += 1;
-      if (minutes >= 60) {
-        minutes = 0;
-      }
-    }
-    if (mode_status == 2) {
-
-      hours += 1;
-      if (hours >= 24) {
-        hours = 0;
-      }
-    }
-    if (mode_status == 3) {
-
-      day += 1;
-      if (day > 31) {
-        day = 1;
-      }
-
-      dayofweek +=1;
-      if(dayofweek>6){
-        dayofweek = 0;
-      }
-      
-    }
-
-    if (mode_status == 4) {
-
-      month += 1;
-      if (month > 12) {
-        month = 1;
-      }
-    }
-
-    if (mode_status == 5) {
-      year += 1;
-    }
-  
-  DisplayTimeBlink(LCD, seconds, minutes, hours, dayofweek, day, month, year );
-  setTime(seconds, minutes, hours, dayofweek, day, month, year);
-  }
-
-  //add decrement button
-
-  if (decrement == HIGH) {
-    if (mode_status == 1) {
-
-      minutes -= 1;
-      if (minutes < 0) {
-        minutes = 59;
-      }
-    }
-    if (mode_status == 2) {
-
-      hours -= 1;
-      if (hours < 0) {
-        hours = 23;
-      }
-    }
-    if (mode_status == 3) {
-
-      day -= 1;
-      if (day < 1) {
-        day = 31;
-      }
-
-      dayofweek +=1;
-      if(dayofweek>6){
-        dayofweek = 0;
-      }
-
-    }
-
-    if (mode_status == 4) {
-
-      month -= 1;
-      if (month < 1) {
-        month = 12;
-      }
-    }
-
-    if (mode_status == 5) {
-      year += 1;
-    }
-  DisplayTimeBlink(LCD, seconds, minutes, hours, dayofweek, day, month, year );
-  setTime(seconds, minutes, hours, dayofweek, day, month, year);
-  }
-
-  //set the new time in normal mode
-  if (set_button == HIGH) {
-    setTime(00, minutes, hours, dayofweek, day, month, year);
-
-    IsEditingMode = 0;
-    readTime(&seconds, &minutes, &hours, &dayofweek, &day, &month, &year );
-
-  }
-
-  if (IsEditingMode == 0) {
-    DisplayTime(LCD, seconds, minutes, hours, dayofweek, day, month, year);
-  }
 
   else {
-    DisplayTimeBlink(LCD, seconds, minutes, hours, dayofweek, day, month, year );
+
+    if (alarm_min == minutes and alarm_hour == hours) { // blinking the LED when time come up
+      start_alarm = 1;
+      digitalWrite(13, HIGH); delay(100); digitalWrite(13, LOW); delay(100);
+    }
+
+    if (mode == HIGH) {
+      IsEditingMode = 1;
+      if (mode_status == 5) {
+        mode_status = 0;
+      }
+      mode_status += 1;// selected minute or hours
+    }
+
+    //add increment button
+    if (increment == HIGH) {
+      if (mode_status == 1) {//minutes editing
+
+        minutes += 1;
+        if (minutes >= 60) {//set the limit for minutes
+          minutes = 0;
+        }
+      }
+      if (mode_status == 2) {//hours editing
+
+        hours += 1;
+        if (hours >= 24) {//set the limit for hours
+          hours = 0;
+        }
+      }
+      if (mode_status == 3) {//day editing
+
+        day += 1;
+        if (day > 31) {// set the day limit
+          day = 1;
+        }
+
+        dayofweek += 1;
+        if (dayofweek > 6) { //set the day of weel limit
+          dayofweek = 0;
+        }
+
+      }
+
+      if (mode_status == 4) {//month editing
+
+        month += 1;
+        if (month > 12) {//set the limit for month
+          month = 1;
+        }
+      }
+
+      if (mode_status == 5) {//year editing
+        year += 1;
+      }
+
+      DisplayTimeBlink(LCD, seconds, minutes, hours, dayofweek, day, month, year );
+      setTime(seconds, minutes, hours, dayofweek, day, month, year);
+    }
+
+    //add decrement button
+
+    if (decrement == HIGH) {//same as previous
+      if (mode_status == 1) {
+
+        minutes -= 1;
+        if (minutes < 0) {
+          minutes = 59;
+        }
+      }
+      if (mode_status == 2) {
+
+        hours -= 1;
+        if (hours < 0) {
+          hours = 23;
+        }
+      }
+      if (mode_status == 3) {
+
+        day -= 1;
+        if (day < 1) {
+          day = 31;
+        }
+
+        dayofweek -= 1;
+        if (dayofweek < 0) {
+          dayofweek = 6;
+        }
+
+      }
+
+      if (mode_status == 4) {
+
+        month -= 1;
+        if (month < 1) {
+          month = 12;
+        }
+      }
+
+      if (mode_status == 5) {
+        year -= 1;
+      }
+      DisplayTimeBlink(LCD, seconds, minutes, hours, dayofweek, day, month, year );
+      setTime(seconds, minutes, hours, dayofweek, day, month, year);//set the new time to RTC
+    }
+
+    //set the new time in normal mode
+    if (set_button == HIGH) {
+      setTime(00, minutes, hours, dayofweek, day, month, year);
+
+      IsEditingMode = 0;
+      readTime(&seconds, &minutes, &hours, &dayofweek, &day, &month, &year );
+
+    }
+
+    if (IsEditingMode == 0) {
+      DisplayTime(LCD, seconds, minutes, hours, dayofweek, day, month, year);
+    }
+
+    else {
+      DisplayTimeBlink(LCD, seconds, minutes, hours, dayofweek, day, month, year );
+    }
   }
-  }
-  }
+}
